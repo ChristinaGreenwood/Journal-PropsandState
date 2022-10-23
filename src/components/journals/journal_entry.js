c8ddafedb996dda52fdff09cefc0cb108456ba55
Christina Greenwood <greenwoodc@me.com>
2022-10-23 13:20:45 -0600

    added css alighed texts and styled buttons

--- src/components/journals/journal_entry.js
+++ src/components/journals/journal_entry.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 // functinal Component (dumb Component )
-export const JournalEntry = props => {
-    return (
-        <div>
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
-        </div>
-    );
+export const JournalEntry = (props) => {
+  return (
+    <div className='journal-entry'>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 };
--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,67 +1,73 @@
-import React, { Component } from 'react'
-import { JournalEntry } from './journal_entry';
+import React, { Component } from "react";
+import { JournalEntry } from "./journal_entry";
 
 const rawJournalData = [
-    { title: 'Post One', content: 'Post Content', status: 'Draft'},
-    { title: 'Post Two', content: 'Post Content', status: 'Published'},
-    { title: 'Post Three', content: 'Post Content', status: 'Published'},
-    { title: 'Post Four', content: 'Post Content', status: 'Published'}
+  { title: "Post One", content: "Post Content", status: "Draft" },
+  { title: "Post Two", content: "Post Content", status: "Published" },
+  { title: "Post Three", content: "Post Content", status: "Published" },
+  { title: "Post Four", content: "Post Content", status: "Published" },
 ];
-     
+
 // class Component
 export default class JournalList extends Component {
-    constructor(props) {
-        super();
+  constructor(props) {
+    super();
 
-        this.state = {
-            journalData: rawJournalData,
-            greeting:'Hey there',
-            isOpen: true
-        };
-        this.clearEntries = this.clearEntries.bind(this) // this is needed for constuctors
-        this.showAllEntries = this.showAllEntries.bind(this) // this is needed for constuctors
-        this.toggleStatus = this.toggleStatus.bind(this) // this is needed for constuctors
+    this.state = {
+      journalData: rawJournalData,
+      greeting: "Hey there",
+      isOpen: true,
+    };
+    this.clearEntries = this.clearEntries.bind(this); // this is needed for constuctors
+    this.showAllEntries = this.showAllEntries.bind(this); // this is needed for constuctors
+    this.toggleStatus = this.toggleStatus.bind(this); // this is needed for constuctors
+  }
+
+  clearEntries() {
+    this.setState({ journalData: [], isOpen: false });
+  }
+
+  showAllEntries() {
+    this.setState({ journalData: rawJournalData, isOpen: true });
+  }
+
+  toggleStatus() {
+    if (this.state.isOpen) {
+      this.setState({ journalData: [], isOpen: false });
+    } else {
+      this.setState({ journalData: rawJournalData, isOpen: true });
     }
+  }
 
-     clearEntries () {
-        this.setState({ journalData: [], isOpen: false });
-    };
-
-    showAllEntries () {
-        this.setState({ journalData: rawJournalData, isOpen: true });
-    };
-
-    toggleStatus () {
-        if (this.state.isOpen) {
-            this.setState({ journalData: [], isOpen: false }) 
-        } else {
-            this.setState({ journalData: rawJournalData, isOpen: true });
-        }
-    };
-    
   render() {
-      const journalEntries = this.state.journalData.map(journalEntry => {
-          return (
-          <div key={journalEntry.title}>
-              <JournalEntry 
-              title={journalEntry.title} 
-              content={journalEntry.content}
-              />
+    const journalEntries = this.state.journalData.map((journalEntry) => {
+      return (
+        <div key={journalEntry.title}>
+          <JournalEntry
+            title={journalEntry.title}
+            content={journalEntry.content}
+          />
         </div>
-          );
-      });
+      );
+    });
 
     return (
-    <div>
+      <div className='journal-list'>
         <h2>{this.props.heading}</h2>
         {journalEntries}
-        
 
-        <button onClick={this.clearEntries}>Clear Journal Entries</button>
-        <button onClick={this.showAllEntries}>Show Entries</button>
-        <button onClick={this.toggleStatus}>Toggle Entries</button>
-
-    </div>
+        <div className='btn-wrapper'>
+          <button className='btn' onClick={this.clearEntries}>
+            Clear Journal Entries
+          </button>
+          <button className='btn' onClick={this.showAllEntries}>
+            Show All Entries
+          </button>
+          <button className='btn' onClick={this.toggleStatus}>
+            Toggle Entries + -
+          </button>
+        </div>
+      </div>
     );
   }
 }
